cfe019fb9daed8eebf2e1f3f090e0979e5878158
Maximo Rosso <maxrosso@uade.edu.ar>
2024-07-13 20:50:15 -0300

    Cambios

--- portfolio/src/components/Skills/skills.jsx
+++ portfolio/src/components/Skills/skills.jsx
@@ -14,14 +14,14 @@ const skills = () => {
                 <img src={UIdesign} alt="DiseñoUI" className="skillBarImg" />
                 <div className="skillBarText">
                     <h2>Diseño UI/UX</h2>
-                    <p>DemoTest</p>
+                    <p>Como diseñador de UI/UX, me especializo en crear interfaces de usuario intuitivas y atractivas que mejoran la experiencia del usuario. Utilizo principios de diseño centrado en el usuario para desarrollar soluciones visuales que no solo sean estéticamente agradables, sino también funcionales y fáciles de usar. </p>
                 </div>
             </div>
             <div className="skillBar">
                 <img src={WebDesign} alt="DiseñoWeb" className="skillBarImg" />
                 <div className="skillBarText">
                     <h2>Diseño Web</h2>
-                    <p>DemoTest</p>
+                    <p>Me especializo en el desarrollo front-end, creando sitios web atractivos y responsivos con HTML, CSS y JavaScript. Me enfoco en experiencias de usuario excepcionales con interfaces visualmente atractivas y funcionales. Actualmente, estoy aprendiendo desarrollo back-end con Node.js y bases de datos para ofrecer soluciones web más completas e integradas.</p>
                 </div>
             </div>
             <div className="skillBar">
